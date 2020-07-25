89921324a01dc99a01dd7de769acc4e8f3da2c88
hans <hans.permana@gmail.com>
2020-07-25 15:37:54 +0700

    pre-build

--- src/com/workshop/Pipeline.groovy
+++ src/com/workshop/Pipeline.groovy
@@ -9,6 +9,7 @@ class Pipeline {
    String docker_user
    String app_port
    String pr_num
+   String dockerTool
  
    Pipeline(
        String repository_name,
@@ -16,15 +17,16 @@ class Pipeline {
        String git_user,
        String docker_user,
        String app_port,
-       String pr_num
+       String pr_num,
+       String dockerTool
    ){
        this.repository_name = repository_name
        this.branch_name = branch_name
-       
        this.git_user = git_user
        this.docker_user = docker_user
        this.app_port = app_port
        this.pr_num = pr_num
+       this.dockerTool = dockerTool
    }
  
 }
--- src/com/workshop/main.groovy
+++ src/com/workshop/main.groovy
@@ -2,12 +2,13 @@
 package com.workshop
  
 import com.workshop.Config
+import com.workshop.stages.*
  
 def main(script) {
    // Object initialization
    c = new Config()
- 
-   // Pipeline specific variable get from injected env
+   sprebuild = new prebuild()
+// Pipeline specific variable get from injected env
    // Mandatory variable will be check at details & validation steps
    def repository_name = ("${script.env.repository_name}" != "null") ? "${script.env.repository_name}" : ""
    def branch_name = ("${script.env.branch_name}" != "null") ? "${script.env.branch_name}" : ""
@@ -15,7 +16,9 @@ def main(script) {
    def docker_user = ("${script.env.docker_user}" != "null") ? "${script.env.docker_user}" : ""
    def app_port = ("${script.env.app_port}" != "null") ? "${script.env.app_port}" : ""
    def pr_num = ("${script.env.pr_num}" != "null") ? "${script.env.pr_num}" : ""
-   
+ 
+   // Initialize docker tools
+   def dockerTool = tool name: 'docker', type: 'dockerTool'
    // Pipeline object
    p = new Pipeline(
        repository_name,
@@ -23,21 +26,21 @@ def main(script) {
        git_user,
        docker_user,
        app_port,
-       pr_num
-       
- 
+       pr_num,
+       dockerTool
    )
  
    ansiColor('xterm') {
-       //stage('Pre Build - Details') {
-           // TODO: Call pre build details function
-       //}
+       stage('Pre Build - Details') {
+           sprebuild.validation(p)
+           sprebuild.details(p)
+       }
  
-       //stage('Pre Build - Checkout & Test') {
-           // TODO: Call pre build checkout & test function
-       //}
- 
-       //stage('Build & Push Image') {
+       stage('Pre Build - Checkout & Test') {
+           sprebuild.checkoutBuildTest(p)
+       }
+
+//stage('Build & Push Image') {
            // TODO: Call build & push image function
        //}
  
@@ -54,5 +57,5 @@ def main(script) {
        //}
    }
 }
-
+ 
 return this
